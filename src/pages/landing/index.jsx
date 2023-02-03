721b328955239ca877c102c08b0e92085d5dd85a
Azeez Samad <seshp@ymail.com>
2023-02-03 10:51:22 +0100

    add scroll down icon

--- src/pages/landing/index.jsx
+++ src/pages/landing/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import arrow from '../../assets/img/arrowDown.svg'
 
 const Landing = () => {
   return (
-    <div>
+    <div className="text-white">
       <div className="lines">
       <div className="bg-lines"></div>
       <div className="bg-lines"></div>
@@ -17,7 +17,7 @@ const Landing = () => {
       <div className="bg-lines"></div>
       <div className="bg-lines"></div>
       </div>
-       <nav className="text-white flex justify-between content-center p-5 mt-4">
+       <nav className="flex justify-between content-center p-5 mt-4">
            <div className="text-2xl">Samad Azeez</div>
            <div>
             <a href="/" className="rounded-full outline hover:bg-gray-600 px-4 py-2">Work</a>
@@ -27,13 +27,17 @@ const Landing = () => {
            </div>
        </nav>
        
-       <section className="text-white pt-10">
+       <section className="pt-10">
            <div className="text-center mt-20 relative">
                <p className="absolute left-[350px] text-2xl">Hi there! 👋🏽 I'm a</p>
            <h1 className="font-bold text-[186px] leading-[100%] tracking-[-.085em] ">front - end developer.<span className="text-[96px]">👨🏽‍💻</span></h1>
-           <div className="w-[624px] h-[472px] bg-[#BD6EC5] rounded-[30px] absolute -z-10 bottom-0 right-[160px]"></div>
+           <div className="w-[624px] h-[472px] bg-[#BD6EC5] rounded-[30px] absolute -z-[2] bottom-0 right-[160px]"></div>
            </div>
        </section>
+       <div className="flex items-center">
+       <img src={arrow} alt="" className="arrow-down"/>
+       <p className="mx-4">Scroll down</p>
+       </div>
        {/* <h1 className="text-5xl font-bold text-white">Building greatness</h1> */}
     </div>
   );
